4de724db9d9b3e81e6b0618393cafc956a474454
Aleksei Chulei <chuley@qlever.ru>
2021-05-23 00:40:46 +0300

    Rename filters titles

--- backend/events/selectors.py
+++ backend/events/selectors.py
@@ -5,11 +5,11 @@ from events.models import Event
 
 class EventsFilter(filters.FilterSet):
 
-    address = filters.CharFilter(field_name='address__city', lookup_expr='icontains')
+    city = filters.CharFilter(field_name='address__city', lookup_expr='icontains')
     topic = filters.CharFilter(field_name='topics__title', lookup_expr='icontains')
-    dos = filters.DateTimeFilter(field_name='dos', lookup_expr='gte')
-    doe = filters.DateTimeFilter(field_name='doe', lookup_expr='lte')
+    start = filters.DateTimeFilter(field_name='dos', lookup_expr='gte')
+    end = filters.DateTimeFilter(field_name='doe', lookup_expr='lte')
 
     class Meta:
         model = Event
-        fields = ('address', 'topic', 'dos', 'doe')
+        fields = ('city', 'topic', 'start', 'end')
