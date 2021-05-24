4818473e2be45b25acf8515ec12d3351783893fa
Aleksei Chulei <chuley@qlever.ru>
2021-05-24 12:59:36 +0300

    refactor settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,17 +140,12 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        # "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
-        # "rest_framework_social_oauth2.authentication.SocialAuthentication",
     ],
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
 
     ),
-    # "AUTH_TOKEN_CLASSES": [
-    #     "rest_framework_simplejwt.tokens.AccessToken",
-    #     "rest_framework_simplejwt.tokens.SlidingToken",
-    # ]
+
 }
 
 LANGUAGE_CODE = 'en-us'
